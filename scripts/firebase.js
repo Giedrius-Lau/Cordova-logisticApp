36382b1fbd5d085a129bf4484939b41aa3fa2c69
zmogus911 <lauruska.giedrius@gmail.com>
2017-06-21 10:57:48 +0300

    first commit

---
+++ scripts/firebase.js
@@ -0,0 +1,26 @@
+﻿
+var config = {
+    apiKey: "AIzaSyAhFnJqAhe-AO9YvsXPF3WH9xIZ2uITy9s",
+    authDomain: "logisticsapp-d0456.firebaseapp.com",
+    databaseURL: "https://logisticsapp-d0456.firebaseio.com",
+    projectId: "logisticsapp-d0456",
+    storageBucket: "logisticsapp-d0456.appspot.com",
+    messagingSenderId: "307825676143"
+};
+firebase.initializeApp(config);
+
+
+
+
+
+function atvykau() {
+    firebase.database().ref().set({
+        Krovinio statusas: 'Atvykau'
+    });
+}
+
+function pradejauKrautis() {
+    firebase.database().ref().set({
+        Krovinio statusas: 'Pradejau krautis'
+    });
+}
---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+﻿
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+
+        document.getElementById('camera').addEventListener('click', function (e) {
+            navigator.camera.getPicture(onSuccess, onFail, {
+                quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL
+            });
+
+        })
+
+    };
+
+    function onPause() {
+    };
+
+    function onResume() {
+    };
+
+    function onSuccess(imageData) {
+        var image = document.getElementById('myImage');
+        image.src = "data:image/jpeg;base64," + imageData;
+
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+})();
