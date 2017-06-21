c3fb74343a9536f276761689c671168998da9b1c
zmogus911 <lauruska.giedrius@gmail.com>
2017-06-21 16:20:04 +0300

    modifyed database structure

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -9,18 +9,74 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var firebaseRef = firebase.database().ref();
 
 
 
-
-function atvykau() {
+function atvykau(clicked) {
     firebase.database().ref().set({
-        Krovinio statusas: 'Atvykau'
+        krovinioStatusas: 'Atvykau į pasikrovimo vietą'
     });
+    $(clicked).addClass('hidden');
+    $("#1").removeClass('hidden');
 }
 
-function pradejauKrautis() {
-    firebase.database().ref().set({
-        Krovinio statusas: 'Pradejau krautis'
+
+function pradejauKrautis(clicked) {
+    firebase.database().ref().update({
+        krovinioStatusas2: 'Pradejau krautis'
     });
+    $(clicked).addClass('hidden');
+    $("#2").removeClass('hidden');
+
 }
+
+function isvykstuIsPasikrovimo(clicked) {
+    firebase.database().ref().update({
+        krovinioStatusas3: 'Išvykau iš pasikrovimo vietos'
+    });
+    $(clicked).addClass('hidden');
+    $("#3").removeClass('hidden');
+
+}
+
+function atvykau2(clicked) {
+    firebase.database().ref().update({
+        krovinioStatusas4: 'Atvykau į išsikrovimo vietą'
+    });
+    $(clicked).addClass('hidden');
+    $("#4").removeClass('hidden');
+
+}
+
+
+function pradejauKrautis2(clicked) {
+    firebase.database().ref().update({
+        krovinioStatusas5: 'Pradejau išsikrovimą'
+    });
+    $(clicked).addClass('hidden');
+    $("#5").removeClass('hidden');
+
+}
+
+function isvykstu(clicked) {
+    firebase.database().ref().update({
+        krovinioStatusas6: 'Išvykau iš išsikrovimo vietos'
+    });
+     $(clicked).addClass('hidden');
+    $("#camera").removeClass('hidden');
+}
+
+
+
+firebase.database().ref().once('value').then((snapshot) => {
+    console.log('Got entire database', snapshot.val());
+}, (e) => {
+    console.log('Unable to fetch value', e);
+    });
+
+firebase.on('value', (snapshot) => {
+    console.log('Got value', snapshot.val());
+});
+
+firebaseRef.update({ krovinioStatusas3: 123123123 });
